da1027c523d7c747f5b32a8a97fd7a8140089315
Kriss <kriss@XIXs.com>
2010-06-08 05:25:56 +0000

    some image processing

--- html/lua/chan/html.lua
+++ html/lua/chan/html.lua
@@ -14,22 +14,43 @@ local f=require("wetgenes.html")
 -----------------------------------------------------------------------------
 html.chan_form=function(d)
 
+	if not d.parent then d.parent="" end
 		
 	return f.replace([[
 	
 <form name="chanpost" id="chanpost" action="/chan/post" method="post" enctype="multipart/form-data">
 
-	<input type="hidden" name="parent" value="0">
+	<input type="hidden" name="parent" value="{parent}">
 
-	<input type="text" name="subject" size="40" maxlength="75" accesskey="s">
-
-	<input type="submit" value="Submit" accesskey="z"> <br/>
+	<input type="text" name="subject" size="40" maxlength="75" accesskey="s"> <br/>
 
 	<textarea name="message" cols="48" rows="4" accesskey="m"></textarea> <br/>
 
 	<input type="file" name="file" size="35" accesskey="f"> 
 
+	<input type="submit" value="Post" accesskey="z"> <br/>
+	
 </form>
+
+]],d)
+
+end
+			
+-----------------------------------------------------------------------------
+--
+--
+-----------------------------------------------------------------------------
+html.chan_post=function(d)
+
+	return f.replace([[
+	
+<div>
+
+EMAIL:{post.cache.email}<br/>
+SUBJECT:{post.cache.subject}<br/>
+BODY:{post.cache.body}<br/>
+
+</div>
 
 ]],d)
 
@@ -38,6 +59,10 @@ end
 			
 			
 
--- something of a dummy module. the html table squirting above is the most important part
 
 module("chan.html")
+
+-- something of a dummy module. the data is set in the global html table
+
+
+
--- html/lua/chan/init.lua
+++ html/lua/chan/init.lua
@@ -11,6 +11,8 @@ local dat=require("wetgenes.aelua.data")
 
 local user=require("wetgenes.aelua.user")
 
+local img=require("wetgenes.aelua.img")
+
 local log=require("wetgenes.aelua.log").log -- grab the func from the package
 
 local wet_string=require("wetgenes.string")
@@ -19,8 +21,11 @@ local serialize=wet_string.serialize
 
 
 -- require all the module sub parts
-require("chan.html")
+local chan_html=require("chan.html")
+local chan_data=require("chan.data")
 
+
+local ipairs=ipairs
 
 local math=math
 local tostring=tostring
@@ -40,6 +45,8 @@ local function put(a,b)
 	srv.put(html.get(a,b))
 end
 
+	if post(srv) then return end -- post handled everything
+
 	srv.set_mimetype("text/html")
 	put("header",{})
 	put("home_bar",{})
@@ -47,10 +54,54 @@ end
 	
 	put("chan_form",{})
 
---	srv.put("<br/><br/>"..tostring(user).."<br/><br/>")
+	srv.put("<br/><br/>"..tostring(srv.posts).."<br/><br/>")
+	srv.put("<br/><br/>"..tostring(srv.uploads).."<br/><br/>")
+	
+	local ts=chan_data.get_threads("new")
+	
+	for i,v in ipairs(ts) do
+
+		put("chan_post",{post=v})
+		
+	end
 
 	put("footer",{})
 	
+end
+
+
+-----------------------------------------------------------------------------
+--
+-- the post function, looks for post params and handles them
+--
+-----------------------------------------------------------------------------
+function post(srv)
+
+	if not user.user then -- must be logged in?
+	
+		return false	
+	end
+	
+	if srv.posts.subject and srv.posts.message then
+	
+		local image=img.get_img(srv.uploads.file.data)
+		
+log(tostring(image))
+	
+		local tab={}
+		
+		tab.subject=srv.posts.subject
+		tab.body=srv.posts.message
+		tab.email=user.user.email
+		tab.ip=srv.ip
+		tab.image=""
+		
+		chan_data.create_thread(tab)
+
+--		srv.redirect("/chan")
+--		return true
+	end
+
 end
 
 
