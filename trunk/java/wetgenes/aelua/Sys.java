d187f491ccf2da14baf0ed0b8a1e44090661e1d0
krissd <krissd@09081f4e-5aab-11b8-2ae6-709035474cd6>
2010-06-18 05:51:15 +0000

    some dice

--- trunk/java/wetgenes/aelua/Sys.java
+++ trunk/java/wetgenes/aelua/Sys.java
@@ -6,6 +6,9 @@ import mnj.lua.LuaJavaCallback;
 import mnj.lua.LuaTable;
 import mnj.lua.Lua;
 
+import java.io.File;
+import java.io.InputStream;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import javax.servlet.http.*;
@@ -64,6 +67,7 @@ public class Sys
 		reg_time(L,lib);
 		reg_clock(L,lib);
 		reg_file_exists(L,lib);
+		reg_file_read(L,lib);
 		
 		return 0;
 	}
@@ -128,6 +132,82 @@ public class Sys
 		return 1;
 		
 	}
+	
+//
+// read this file as a bytearray
+//
+	public void reg_file_read(Lua L,Object lib)
+	{ 
+		final Sys _base=this;
+		L.rawSet(lib, "file_read", new LuaJavaCallback(){ Sys base=_base; public int luaFunction(Lua L){ return base.file_read(L); } });
+	}
+	public int file_read(Lua L)
+	{
+		byte[] b;
+	
+		String s=L.checkString(1);
+		try
+		{
+			b = getBytesFromFile( new File(s) );
+		}
+		catch (IOException e_)
+		{
+			L.push( false );
+			return 1;
+		}
+		
+		L.push( b );
+		return 1;
+	}
+	
+    /**
+     * Returns the contents of the file in a byte array
+     * @param file File this method should read
+     * @return byte[] Returns a byte[] array of the contents of the file
+     */
+    private static byte[] getBytesFromFile(File file) throws IOException {
+
+        InputStream is = new FileInputStream(file);
+//        System.out.println("\nDEBUG: FileInputStream is " + file);
+
+        // Get the size of the file
+        long length = file.length();
+//        System.out.println("DEBUG: Length of " + file + " is " + length + "\n");
+
+        /*
+         * You cannot create an array using a long type. It needs to be an int
+         * type. Before converting to an int type, check to ensure that file is
+         * not loarger than Integer.MAX_VALUE;
+         */
+        if (length > Integer.MAX_VALUE) {
+            throw new IOException("File is too large to process " + file.getName());
+//            return null;
+        }
+
+        // Create the byte array to hold the data
+        byte[] bytes = new byte[(int)length];
+
+        // Read in the bytes
+        int offset = 0;
+        int numRead = 0;
+
+         while ( (offset < bytes.length)
+                &&
+                ( (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) ) {
+
+            offset += numRead;
+
+        }
+
+        // Ensure all the bytes have been read in
+        if (offset < bytes.length) {
+            throw new IOException("Could not completely read file " + file.getName());
+        }
+
+        is.close();
+        return bytes;
+
+    }
 }
 
 
--- trunk/mods/dice/lua/init.lua
+++ trunk/mods/dice/lua/init.lua
@@ -23,10 +23,12 @@ local html=require("dice.html")
 
 local math=math
 local string=string
+local table=table
 
 local ipairs=ipairs
 local tostring=tostring
 local tonumber=tonumber
+local type=type
 
 module("dice")
 
@@ -40,6 +42,9 @@ module("dice")
 function serv(srv)
 	if post(srv) then return end -- post handled everything
 
+	local slash=srv.url_slash[ srv.url_slash_idx ]
+	if slash=="image" then return image(srv) end
+		
 local function put(a,b)
 	b=b or {}
 	b.srv=srv
@@ -53,7 +58,34 @@ end
 	put("user_bar",{})
 	
 	put("dice_test",{})
-
+	
+	local count=1
+	local sides=6
+		
+	if slash and slash~="" then -- requested format, eg 2d6
+	
+		local ds=wet_string.str_split("d",slash)
+		count=math.floor( tonumber(ds[1] or 1) or 1 )
+		sides=math.floor( tonumber(ds[2] or 6) or 6 )
+		
+	end
+	
+	if count<1 then count=1 end
+	if count>10 then count=10 end
+	if sides<1 then sides=1 end
+	if sides>20 then sides=20 end
+	
+	put("You have requested {count} dice with {sides} sides.<br/>",{count=count,sides=sides})
+	
+	local rolls={}
+	for i=1,count do
+		rolls[i]=math.random(1,sides)
+	end
+	
+	local imgid=sides.."/"..table.concat(rolls,".")
+	
+	put("<a href=\"/dice/image/{imgid}.jpg\"><img src=\"/dice/image/{imgid}.jpg\"/></a><br/>",{count=count,sides=sides,imgid=imgid})
+	
 	put("footer",{})
 	
 end
@@ -70,3 +102,33 @@ function post(srv)
 
 end
 
+-----------------------------------------------------------------------------
+--
+-- return an image
+--
+-----------------------------------------------------------------------------
+function image(srv)
+
+	local base=tonumber(srv.url_slash[ srv.url_slash_idx+1 ] or 6) or 6
+	local slash=srv.url_slash[ srv.url_slash_idx+2 ]
+
+	local code=wet_string.str_split(".",slash)
+	local nums={}
+	for i=1,#code do
+		local n=tonumber(code[i])
+		if n then table.insert(nums,n) end
+	end
+	
+	local imgs={}
+	local comp={width=#nums*100, height=100, color=0, format="JPEG"}
+	for i=1,#nums do local v=nums[i]
+		if not imgs[v] then imgs[v]=img.get(sys.file_read("art/dice/d6."..v..".png")) end -- load image
+		table.insert(comp,{imgs[v],100*(i-1),0,1,"TOP_LEFT"})
+	end
+
+	local t2=img.composite(comp)
+	
+	srv.set_mimetype( "image/"..string.lower(t2.format) )
+	srv.put(t2.data)
+		
+end
