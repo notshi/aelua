150ae50e0c2fa102fa428998386fc788117665bf
krissd <krissd@09081f4e-5aab-11b8-2ae6-709035474cd6>
2010-07-05 18:56:28 +0000

    added splash screen

---
+++ trunk/shit-product/html/lua/opts.lua
@@ -0,0 +1,20 @@
+
+local os=os
+local dat=require("wetgenes.aelua.data")
+local cache=require("wetgenes.aelua.cache")
+
+local wet_string=require("wetgenes.string")
+local str_split=wet_string.str_split
+local serialize=wet_string.serialize
+
+module("opts")
+
+mods={}
+
+mods.console={}
+
+mods.console.input=
+[[
+]]
+
+
