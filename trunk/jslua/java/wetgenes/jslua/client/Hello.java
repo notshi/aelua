bc2651383191cb16fffb20236175614c6fa98d27
krissd <krissd@09081f4e-5aab-11b8-2ae6-709035474cd6>
2010-07-10 01:37:29 +0000

    Maybe we can get jill to build with GWT? Worth a try, hence all this junk I just added...

---
+++ trunk/jslua/java/wetgenes/jslua/client/Hello.java
@@ -0,0 +1,44 @@
+
+package wetgenes.jslua.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+
+import mnj.lua.*;
+
+
+/**
+ * HelloWorld application.
+ */
+
+public class Hello implements EntryPoint {
+
+  public void onModuleLoad() {
+    Button b = new Button("Click here bitches", new ClickHandler() {
+      public void onClick(ClickEvent event) {
+		  
+		Lua L = new Lua();
+		
+		BaseLib.open(L);
+		PackageLib.open(L);
+		StringLib.open(L);
+		TableLib.open(L);
+		MathLib.open(L);
+		OSLib.open(L);
+
+		L.loadString("test='poop on you'","test");
+		L.call(0,0);
+
+        Window.alert( (String) L.rawGet(L.getGlobals(), "test") );
+      }
+    });
+
+    RootPanel.get().add(b);
+  }
+}
+
+
