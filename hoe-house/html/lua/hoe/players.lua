f0016b5fd0f00a4fb4b893e652c405701d1276c4
Kriss <kriss@XIXs.com>
2010-07-12 03:59:26 +0000

    scarescrows renamed to bros and basic working is working

--- hoe-house/html/lua/hoe/players.lua
+++ hoe-house/html/lua/hoe/players.lua
@@ -27,6 +27,7 @@ local string=string
 local table=table
 
 local ipairs=ipairs
+local pairs=pairs
 local tostring=tostring
 local tonumber=tonumber
 local type=type
@@ -39,7 +40,7 @@ module("hoe.players")
 
 --------------------------------------------------------------------------------
 --
--- serving flavour can be used to create a subgame of a certain flavour
+-- serving flavour can be used to create a subgame of a differnt flavour
 -- make sure we incorporate flavour into the name of our stored data types
 --
 --------------------------------------------------------------------------------
@@ -72,7 +73,7 @@ function create(H)
 	p.bux=0
 	p.houses=1
 	p.hoes=0
-	p.scarecrows=0
+	p.bros=0
 	p.gloves=100
 	p.sticks=0
 	p.manure=0
@@ -117,7 +118,7 @@ function check(H,ent)
 
 -- score can be rebuilt from all other values
 	
-	c.score= ( c.houses * 50000 ) + ( c.hoes * 1000 ) + ( c.scarecrows * 100 ) + ( c.bux ) + ( c.manure )
+	c.score= ( c.houses * 50000 ) + ( c.hoes * 1000 ) + ( c.bros * 100 ) + ( c.bux ) + ( c.manure )
 
 	return ent
 end
@@ -210,7 +211,7 @@ function join(H,user)
 				-- this bit deals with the fact that we have two transactions and that
 				-- tu may get commited then tp might fail
 				if ud.player_id then -- already joined?
-					p=get_id(H,ud.player_id)
+					p=get_id(H,ud.player_id) -- does the player exist? (no transaction needed)
 					if p and p.cache.email==user.cache.email then -- check that link to player is good
 						tu.rollback()
 						tp.rollback()
@@ -253,7 +254,7 @@ function list(H,opts,t)
 		limit=10,
 		offset=0,
 			{"filter","round_id","==",H.round.key.id},
-			{"sort","updated","DESC"},
+			{"sort","score","DESC"},
 		})
 		
 	for i=1,#r do local v=r[i]
@@ -276,5 +277,49 @@ function count_ticks(from,stop,step)
 	local stop=math.floor(stop/step)
 
 	return stop-from
+end
+
+--------------------------------------------------------------------------------
+--
+-- adjust a player by a table or a function
+--
+--------------------------------------------------------------------------------
+function adjust(H,id,by)
+
+	local f=function(H,p)
+		if by.energy<0 then
+			if p.energy<(-by.energy) then -- not enough energy to perform
+				return false
+			end
+		end
+		for i,v in pairs(by) do
+			if type(v)=="number" then -- add it
+				p[i]=(p[i] or 0)+v
+				if p[i]<0 then p[i]=0 end -- not allowed to go bellow 0
+			else -- just set it
+				p[i]=v
+			end
+		end
+		return true
+	end
+
+	if type(id)=="table" then id=id.key.id end -- can turn a player into an id
+	if type(by)=="function" then f=func end -- more control
+		
+	for retry=1,10 do
+		local t=dat.begin()
+		local p=get_id(H,id,t)
+		if p then
+			if not f(H,p.cache) then return false end -- hard fail, possibly due to lack of energy
+			check(H,p) -- also update the score
+			if put(H,p,t) then -- player put ok
+				if t.commit() then -- success
+					return p -- return the adjusted player
+				end
+			end
+		end
+		t.rollback() -- undo everything ready to try again
+	end
+	
 end
 
