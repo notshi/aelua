0b4b206221887d74024a7edeeccfc509e9a0e3b9
Kriss <kriss@XIXs.com>
2010-07-18 22:43:47 +0000

    added some cgilua code

---
+++ aelua/lua/wetgenes/cgilua/session.lua
@@ -0,0 +1,103 @@
+--
+-- Mysql helper functions
+--
+
+require"cgilua.cookies"
+
+local sql=require "wetgenes.cgilua.mysql"
+
+
+local type,ipairs=type,ipairs
+
+local string=string
+
+
+local wet = wetgenes.cgilua
+local cfg = cfg
+
+
+local cgilua=cgilua
+
+local dbg=dbg or function(s) cgilua.errorlog(s) end
+
+module("wetgenes.cgilua.session")
+
+
+
+-----------------------------------------------------------------------------
+--
+-- log in using a session
+-- if need is set to true then redirect to a login page as a login is needed
+--
+-----------------------------------------------------------------------------
+function login(need)
+
+local sess=cgilua.cookies.get(cfg.cookie_session)
+
+local redirect="http://join."..cfg.base_domain.."?redirect="..wet.cgi.url
+
+
+user={}
+
+
+	if sess then
+
+		sess=string.gsub(sess, "[^0-9a-zA-Z]+", "" )
+
+
+		local info=wet.mysql.execute([[SELECT
+				u.alias, u.posts_ppg, u.time_zone, u.sig, u.last_visit, u.last_read, u.cat_collapse_status, u.users_opt,
+				u.ignore_list, u.buddy_list, u.id, u.group_leader_list, u.email, u.login, u.sq, u.ban_expiry
+				, u.avatar_loc , u.referer_id , s.time_sec , s.sys_id
+			FROM ]]..cfg.mysql_prefix_fud..[[ses s
+				INNER JOIN ]]..cfg.mysql_prefix_fud..[[users u ON u.id=(CASE WHEN s.user_id>2000000000 THEN 1 ELSE s.user_id END)
+			WHERE ses_id=']]..sess..[[']])
+
+		local tab=wet.mysql.named(info,1)
+
+		if tab then -- session exists
+		
+			if wet.cgi.ip==tab.sys_id then -- mild security fix, session is locked to ip
+			
+			
+				user.fud=tab
+				
+				if tab.login=="XIX" or tab.login=="shi" then -- flag admin logins
+					tab.admin=true
+				end
+			
+--	dbg("success")
+	
+			else
+
+				if need then
+					wet.cgi.redirect(redirect)
+				end
+--	dbg("fail bad ip")
+
+			end
+			
+			
+		else -- user does not exist
+		
+--	dbg("fail bad sesson")
+			
+			if need then
+				wet.cgi.redirect(redirect)
+			end
+			
+		end
+
+	else
+	
+		if need then
+			wet.cgi.redirect(redirect)
+		end
+			
+--	dbg("fail no session")
+	
+	end
+
+end
+
+
