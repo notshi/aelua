cd9e8d5530cb9a799b9b923160addb08db9b5b69
krissd <krissd@09081f4e-5aab-11b8-2ae6-709035474cd6>
2010-07-19 02:42:14 +0000

    working?

--- trunk/aelua/lua/wetgenes/cgilua/init.lua
+++ trunk/aelua/lua/wetgenes/cgilua/init.lua
@@ -8,25 +8,18 @@ local cgilua=cgilua
 
 local string=string
 local table=table
+local math=math
 
 local type=type
 local pairs=pairs
 local setfenv=setfenv
 
+local socket=require("socket")
 
+local misc=require("wetgenes.cgilua.misc")
 
 module("wetgenes.cgilua")
 wet=_M -- wet should point to this module
-
------------------------------------------------------------------------------
---
--- wrap everything
---
------------------------------------------------------------------------------
-function setup() -- call setup once to get a global wet cgilua wrapper for this request
-
-local _setup={}
-setfenv(1, _setup)
 
 -----------------------------------------------------------------------------
 --
@@ -41,73 +34,11 @@ dbg=function(s)
 	cgilua.put(s)
 end
 
-
------------------------------------------------------------------------------
---
--- Build some always useful information about this reuest
---
------------------------------------------------------------------------------
-
-path=cgilua.servervariable("PATH_INFO")
-
-server=cgilua.servervariable("SERVER_NAME")
-
-query=cgilua.servervariable("QUERY_STRING")
-
-ip=cgilua.servervariable("REMOTE_ADDR")
-ipnum=wet.misc.ipstr_to_number(ip)
-
-url="http://"..server..path
-
-if query and query~="" then url=url.."?"..query end
-
-slash=wet.misc.str_split("/",cgilua.script_vpath)
-if slash[1] then table.remove(slash,1) end -- first is always ""
-
--- url is now probably correct if we need to redirect to ourselves
-
-
--- the lua query/post are dangerous as they may contain tables...
--- this creates safe copies that are all strings
-
-gets={}
-
-for i,v in pairs(cgilua.QUERY) do
-
-	if type(i)=="string" and type(v)=="string" then
-
-		gets[i]=v
-
-	end
-end
-
-posts={}
-
-for i,v in pairs(cgilua.POST) do
-
-	if type(i)=="string" and type(v)=="string" then
-
-		posts[i]=v
-
-	end
-end
-
-json=nil
-
-if gets.fmt=="json" or posts.fmt=="json" then -- a special json request, start building the return
-
-	json={}
-	json.doups={}
-
-end
-
 -----------------------------------------------------------------------------
 --
 -- Set content type and remember headers have been sent
 --
 -----------------------------------------------------------------------------
-headers_sent=false
-
 function contentheader(type, subtype)
 
 	if headers_sent then return end -- so this is safe to call multiple times
@@ -142,7 +73,71 @@ end
 
 -----------------------------------------------------------------------------
 --
--- the above functions live in this wrapper
+-- Build some always useful information about this request
+-- into this module
 --
 -----------------------------------------------------------------------------
-return _setup
+function setup() -- call setup once to set global values for this request?
+
+	start_time=socket.gettime()
+	query_count=0
+	math.randomseed( math.floor(start_time*1000) )
+	math.random()
+
+	headers_sent=false
+
+	path=cgilua.servervariable("PATH_INFO")
+
+	server=cgilua.servervariable("SERVER_NAME")
+
+	query=cgilua.servervariable("QUERY_STRING")
+
+	ip=cgilua.servervariable("REMOTE_ADDR")
+	ipnum=misc.ipstr_to_number(ip)
+
+	url="http://"..server..path
+
+	if query and query~="" then url=url.."?"..query end
+
+	slash=misc.str_split("/",cgilua.script_vpath)
+	if slash[1] then table.remove(slash,1) end -- first is always ""
+
+	-- url is now probably correct if we need to redirect to ourselves
+
+
+	-- the lua query/post are dangerous as they may contain tables...
+	-- this creates safe copies that are all strings
+
+	gets={}
+
+	for i,v in pairs(cgilua.QUERY) do
+
+		if type(i)=="string" and type(v)=="string" then
+
+			gets[i]=v
+
+		end
+	end
+
+	posts={}
+
+	for i,v in pairs(cgilua.POST) do
+
+		if type(i)=="string" and type(v)=="string" then
+
+			posts[i]=v
+
+		end
+	end
+
+	json=nil
+
+	if gets.fmt=="json" or posts.fmt=="json" then -- a special json request, start building the return
+
+		json={}
+		json.doups={}
+
+	end
+	
+	return _M
+end
--- trunk/aelua/lua/wetgenes/cgilua/session.lua
+++ trunk/aelua/lua/wetgenes/cgilua/session.lua
@@ -4,7 +4,7 @@
 
 require"cgilua.cookies"
 
-local sql=require "wetgenes.cgilua.mysql"
+local sql=require("wetgenes.cgilua.mysql")
 
 
 local type,ipairs=type,ipairs
@@ -34,7 +34,7 @@ function login(need)
 
 local sess=cgilua.cookies.get(cfg.cookie_session)
 
-local redirect="http://join."..cfg.base_domain.."?redirect="..wet.cgi.url
+local redirect="http://join."..cfg.base_domain.."?redirect="..wet.url
 
 
 user={}
@@ -45,7 +45,7 @@ user={}
 		sess=string.gsub(sess, "[^0-9a-zA-Z]+", "" )
 
 
-		local info=wet.mysql.execute([[SELECT
+		local info=sql.execute([[SELECT
 				u.alias, u.posts_ppg, u.time_zone, u.sig, u.last_visit, u.last_read, u.cat_collapse_status, u.users_opt,
 				u.ignore_list, u.buddy_list, u.id, u.group_leader_list, u.email, u.login, u.sq, u.ban_expiry
 				, u.avatar_loc , u.referer_id , s.time_sec , s.sys_id
@@ -53,11 +53,11 @@ user={}
 				INNER JOIN ]]..cfg.mysql_prefix_fud..[[users u ON u.id=(CASE WHEN s.user_id>2000000000 THEN 1 ELSE s.user_id END)
 			WHERE ses_id=']]..sess..[[']])
 
-		local tab=wet.mysql.named(info,1)
+		local tab=sql.named(info,1)
 
 		if tab then -- session exists
 		
-			if wet.cgi.ip==tab.sys_id then -- mild security fix, session is locked to ip
+			if wet.ip==tab.sys_id then -- mild security fix, session is locked to ip
 			
 			
 				user.fud=tab
@@ -71,7 +71,7 @@ user={}
 			else
 
 				if need then
-					wet.cgi.redirect(redirect)
+					wet.redirect(redirect)
 				end
 --	dbg("fail bad ip")
 
@@ -83,7 +83,7 @@ user={}
 --	dbg("fail bad sesson")
 			
 			if need then
-				wet.cgi.redirect(redirect)
+				wet.redirect(redirect)
 			end
 			
 		end
@@ -91,12 +91,14 @@ user={}
 	else
 	
 		if need then
-			wet.cgi.redirect(redirect)
+			wet.redirect(redirect)
 		end
 			
 --	dbg("fail no session")
 	
 	end
+	
+	return user
 
 end
 
