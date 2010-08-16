0d17eb6d10d6c16fec1f21ecd2b84d622c5bda0a
Kriss <kriss@XIXs.com>
2010-08-16 23:31:19 +0000

    smart recursive replace + macros

--- aelua/lua/wetgenes/html.lua
+++ aelua/lua/wetgenes/html.lua
@@ -6,56 +6,15 @@ local type=type
 local tostring=tostring
 local setmetatable=setmetatable
 
+-- my string functions
+local str=require("wetgenes.string")
+
 module("wetgenes.html")
 
-
------------------------------------------------------------------------------
 --
--- replace tags in {} in the string with data provided
--- allow sub table look up with a.b notation in the name
+-- use the replace function from wetgenes.string
 --
------------------------------------------------------------------------------
-function replace(a,d)
-
-return (string.gsub( a , "{([%w%._%-]-)}" , function(a) -- find only words and "._-" tightly encased in {}
--- this means that almost all legal use of {} in javascript will not match at all.
--- Even when it does (probably as a "{}") then it is unlikley to accidently find anything in the d table
--- so the text will just be returned as is.
--- So it may not be safe, but it is simple to understand and perfecty fine under most use cases.
-
-	local f
-	
-	f=function(a,d) -- look up a in table d
-	
---		if string.len(a)>256 then return "{"..a.."}" end -- do not even try with really large strings?
-	
-		local t=d[a]
-		if t then
-			if type(t)=="table" then return table.concat(t) end -- if a table then join its contents
-			return tostring(t) -- simple find, make sure we return a string
-		end
-		
-		local a1,a2=string.find(a, "%.") -- try and split on first "."
-		if not a1 then return "{"..a.."}" end -- didnt find a dot so return look up value keeping {}
-		
-		a1=string.sub(a,1,a1-1) -- the bit before the .
-		a2=string.sub(a,a2+1) -- the bit after the .
-		
-		local dd=d[a1] -- use the bit befre the dot to find the sub table
-		
-		if type(dd)=="table" then -- check we got a table
-			return f(a2,dd) -- tail call this function
-		end
-		
-		return "{"..a.."}" -- couldnt find anything return input string
-	end
-
-	return f(a,d)
-	
-end ))
-
-end
-
+replace=str.replace
 
 
 -----------------------------------------------------------------------------
@@ -70,7 +29,6 @@ end
 get=function(html,src,env)
 
 	local new_env={}
-
 	if env then setmetatable(new_env,{__index=env})	end -- wrap to protect
 
 	if html[src] then src=html[src] end
