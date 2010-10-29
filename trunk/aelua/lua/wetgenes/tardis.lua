75efb2c2356e3b9f122288d77f114e3776243b43
krissd <krissd@09081f4e-5aab-11b8-2ae6-709035474cd6>
2010-10-29 04:51:55 +0000

    working m4 inverse?

--- trunk/aelua/lua/wetgenes/tardis.lua
+++ trunk/aelua/lua/wetgenes/tardis.lua
@@ -5,14 +5,15 @@
 -- a lua library for manipulating time and space
 -- pure lua by default and opengl in flavour
 --
--- recoil in terror as we use two glyph names to describe table structures
+-- recoil in terror as we use two glyph names to describe structures
+-- whilst typing in random strings of numbers that may or may not contain tyops
 --
 -- v# vector [#]
 -- m# matrix [#][#]
 -- q4 quaternion (yeah its just a repackaged v4)
 --
 -- each class is a table of # values [1] to [#] , just access them directly
--- they are number streams formated the same way as opengl
+-- they are number streams formated the same way as opengl (row-major)
 -- metatables are used to provide advanced functionality
 
 
@@ -20,6 +21,7 @@ local math=require("math")
 local table=require("table")
 local string=require("string")
 
+local unpack=unpack
 local getmetatable=getmetatable
 local setmetatable=setmetatable
 local type=type
@@ -28,7 +30,6 @@ local ipairs=ipairs
 local tostring=tostring
 local tonumber=tonumber
 local require=require
-
 
 module(...)
 local _M=require(...) -- do not rely on any side effects of module
@@ -69,8 +70,8 @@ function array.__tostring(it) -- these classes are all just arrays of numbers
 	t[#t+1]=mtype(it)
 	t[#t+1]="{"
 	for i=1,#it do
-		t[#t+1]=tostring(it.i)
-		if i~=#t then t[#t+1]=", " end
+		t[#t+1]=tostring(it[i])
+		if i~=#it then t[#t+1]=", " end
 	end
 	t[#t+1]="}"
 	return table.concat(t)
@@ -96,12 +97,148 @@ end
 
 class("m2",array)
 function m2.new(...) return setmetatable({0,0,0,0},m2):set(...) end
+function m2.determinant(it)
+	return	 ( it[ 1 ]*it[ 2+2 ] )
+			+( it[ 2 ]*it[ 2+1 ] )
+			-( it[ 1 ]*it[ 2+1 ] )
+			-( it[ 2 ]*it[ 2+1 ] )
+end
+function m2.transpose(it)
+	return	 it[1],it[2+1], it[2],it[2+2]
+end
+function m2.minor_xy(it,x,y)
+	local t={}
+	for ix=1,2 do
+		for iy=1,2 do
+			if (ix~=x) and (iy~=y) then
+				t[#t+1]=it(ix+((iy-1)*2))
+			end
+		end
+	end
+	return t[1]
+end
+function m2.cofactor(it)
+	local t={}
+	for ix=1,2 do
+		for iy=1,2 do
+			if (ix~=x) and (iy~=y) then
+				t[#t+1]=m2.minor_xy(it,ix,iy)
+				if ((ix+iy)%2)==1 then t[#t]=-t[#t] end
+			end
+		end
+	end
+	return unpack(t)
+end
+function m2.adjugate(it)
+	return m2.transpose{m2.cofactor(it)}
+end
+function m2.scale(it,s)
+	return it[1]*s,it[2]*s, it[2+1]*s,it[2+2]*s
+end
+function m2.inverse(it)
+	local ood=1/m2.determinant(it)	
+	return m2.scale({m2.cofactor{m2.transpose(it)}},ood)
+end
 
 class("m3",m2)
 function m3.new(...) return setmetatable({0,0,0,0,0,0,0,0,0},m3):set(...) end
+function m3.determinant(it)
+	return	 ( it[ 1 ]*it[ 3+2 ]*it[ 6+3 ] )
+			+( it[ 2 ]*it[ 3+3 ]*it[ 6+1 ] )
+			+( it[ 3 ]*it[ 3+1 ]*it[ 6+2 ] )
+			-( it[ 1 ]*it[ 3+3 ]*it[ 6+2 ] )
+			-( it[ 2 ]*it[ 3+1 ]*it[ 6+3 ] )
+			-( it[ 3 ]*it[ 3+2 ]*it[ 6+1 ] )
+end
+function m3.transpose(it)
+	return	 it[1],it[3+1],it[6+1], it[2],it[3+2],it[6+2], it[3],it[3+3],it[6+3]
+end
+function m3.minor_xy(it,x,y)
+	local t={}
+	for ix=1,3 do
+		for iy=1,3 do
+			if (ix~=x) and (iy~=y) then
+				t[#t+1]=it(ix+((iy-1)*3))
+			end
+		end
+	end
+	return m2.determinant(t)
+end
+function m3.cofactor(it)
+	local t={}
+	for ix=1,3 do
+		for iy=1,3 do
+			if (ix~=x) and (iy~=y) then
+				t[#t+1]=m3.minor_xy(it,ix,iy)
+				if ((ix+iy)%2)==1 then t[#t]=-t[#t] end
+			end
+		end
+	end
+	return unpack(t)
+end
+function m3.adjugate(it)
+	return m3.transpose{m3.cofactor(it)}
+end
+function m3.scale(it,s)
+	return it[1]*s,it[2]*s,it[3]*s, it[3+1]*s,it[3+2]*s,it[3+3]*s, it[6+1]*s,it[6+2]*s,it[6+3]*s
+end
+function m3.inverse(it)
+	local ood=1/m3.determinant(it)	
+	return m3.scale({m3.cofactor{m3.transpose(it)}},ood)
+end
 
 class("m4",m3)
 function m4.new(...) return setmetatable({0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},m4):set(...) end
+function m4.determinant(it)
+return	(it[ 4 ] * it[ 4+3 ] * it[ 8+2 ] * it[ 12+1 ])-(it[ 3 ] * it[ 4+4 ] * it[ 8+2 ] * it[ 12+1 ])-
+		(it[ 4 ] * it[ 4+2 ] * it[ 8+3 ] * it[ 12+1 ])+(it[ 2 ] * it[ 4+4 ] * it[ 8+3 ] * it[ 12+1 ])+
+		(it[ 3 ] * it[ 4+2 ] * it[ 8+4 ] * it[ 12+1 ])-(it[ 2 ] * it[ 4+3 ] * it[ 8+4 ] * it[ 12+1 ])-
+		(it[ 4 ] * it[ 4+3 ] * it[ 8+1 ] * it[ 12+2 ])+(it[ 3 ] * it[ 4+4 ] * it[ 8+1 ] * it[ 12+2 ])+
+		(it[ 4 ] * it[ 4+1 ] * it[ 8+3 ] * it[ 12+2 ])-(it[ 1 ] * it[ 4+4 ] * it[ 8+3 ] * it[ 12+2 ])-
+		(it[ 3 ] * it[ 4+1 ] * it[ 8+4 ] * it[ 12+2 ])+(it[ 1 ] * it[ 4+3 ] * it[ 8+4 ] * it[ 12+2 ])+
+		(it[ 4 ] * it[ 4+2 ] * it[ 8+1 ] * it[ 12+3 ])-(it[ 2 ] * it[ 4+4 ] * it[ 8+1 ] * it[ 12+3 ])-
+		(it[ 4 ] * it[ 4+1 ] * it[ 8+2 ] * it[ 12+3 ])+(it[ 1 ] * it[ 4+4 ] * it[ 8+2 ] * it[ 12+3 ])+
+		(it[ 2 ] * it[ 4+1 ] * it[ 8+4 ] * it[ 12+3 ])-(it[ 1 ] * it[ 4+2 ] * it[ 8+4 ] * it[ 12+3 ])-
+		(it[ 3 ] * it[ 4+2 ] * it[ 8+1 ] * it[ 12+4 ])+(it[ 2 ] * it[ 4+3 ] * it[ 8+1 ] * it[ 12+4 ])+
+		(it[ 3 ] * it[ 4+1 ] * it[ 8+2 ] * it[ 12+4 ])-(it[ 1 ] * it[ 4+3 ] * it[ 8+2 ] * it[ 12+4 ])-
+		(it[ 2 ] * it[ 4+1 ] * it[ 8+3 ] * it[ 12+4 ])+(it[ 1 ] * it[ 4+2 ] * it[ 8+3 ] * it[ 12+4 ])	
+end
+function m4.transpose(it)
+	return	 it[1],it[4+1],it[8+1],it[12+1], it[2],it[4+2],it[8+2],it[12+2], it[3],it[4+3],it[8+3],it[12+3], it[4],it[4+4],it[8+4],it[12+4]
+end
+function m4.minor_xy(it,x,y)
+	local t={}
+	for ix=1,4 do
+		for iy=1,4 do
+			if (ix~=x) and (iy~=y) then
+				t[#t+1]=it[ix+((iy-1)*4)]
+			end
+		end
+	end
+	return m3.determinant(t)
+end
+function m4.cofactor(it)
+	local t={}
+	for ix=1,4 do
+		for iy=1,4 do
+			if (ix~=x) and (iy~=y) then
+				t[#t+1]=m4.minor_xy(it,ix,iy)
+				if ((ix+iy)%2)==1 then t[#t]=-t[#t] end
+			end
+		end
+	end
+	return unpack(t)
+end
+function m4.adjugate(it)
+	return m4.transpose{m4.cofactor(it)}
+end
+function m4.scale(it,s)
+	return it[1]*s,it[2]*s,it[3]*s,it[4]*s, it[4+1]*s,it[4+2]*s,it[4+3]*s,it[4+4]*s, it[8+1]*s,it[8+2]*s,it[8+3]*s,it[8+4]*s, it[12+1]*s,it[12+2]*s,it[12+3]*s,it[12+4]*s
+end
+function m4.inverse(it)
+	local ood=1/m4.determinant(it)	
+	return m4.scale({m4.cofactor{m4.transpose(it)}},ood)
+end
 
 
 
