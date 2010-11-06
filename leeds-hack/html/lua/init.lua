b6656cf82c388ddb5d3169fed67383d5264c0274
Kriss <kriss@XIXs.com>
2010-11-06 12:31:43 +0000

    a little project for http://www.leedshack.com

---
+++ leeds-hack/html/lua/init.lua
@@ -0,0 +1,9 @@
+local basic=require("wetgenes.aelua.basic")
+
+-- shove this basic functions into the global name space
+-- they will work with the opts to serv this app as needed
+
+serv_fail=basic.serv_fail
+
+serv=basic.serv
+
