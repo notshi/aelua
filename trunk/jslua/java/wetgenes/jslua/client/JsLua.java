add42ecb69f51f611ba076897faea6041305ca55
krissd <krissd@09081f4e-5aab-11b8-2ae6-709035474cd6>
2010-11-07 00:25:17 +0000

    half broken

--- trunk/jslua/java/wetgenes/jslua/client/JsLua.java
+++ trunk/jslua/java/wetgenes/jslua/client/JsLua.java
@@ -51,7 +51,7 @@ public class JsLua implements EntryPoint {
     build_bridge();
   }
   
-    public static Lua lua_new() {
+    public static Lua lua_create() {
 		
  		Lua L = new Lua();
 		
@@ -65,10 +65,74 @@ public class JsLua implements EntryPoint {
 		return L;
 	}
 
-	public static native void build_bridge() /*-{
-		$wnd.lua_new = function() {
-			return @wetgenes.jslua.client.JsLua::lua_new();
+    public static String lua_get(Lua L, String n) {
+		
+		return (String) L.rawGet(L.getGlobals(), n);
+	}
+	
+    public static void lua_set(Lua L, String n, String v) {
+		
+		L.rawSet(L.getGlobals(), n,v);
+	}
+
+// just get/set globals and call for pram passing
+    public static String lua_dostring(Lua L, String s, String n) {
+		
+		String r="";
+		
+		try
+		{
+			L.loadString(s,n);
+			L.call(0,1);
 		}
+		catch(Exception e)
+		{
+			r=e.toString();
+		}
+		
+		if( L.type(-1) == L.TSTRING ) // got a string, we can only return strings
+		{
+			r=(String)L.value(-1);
+		}
+		
+		L.pop(1);
+		
+		return r;
+	}
+
+// load a string, and set the result into the package.preload of the given name
+    public static void lua_preloadstring(Lua L, String s, String n) {
+		
+		Object o=L.rawGet(L.getGlobals(), "package");
+		o=L.rawGet(o, "preload");
+		
+		L.loadString(s,n); // push on stack
+		Object f=L.value(-1); // get from stack
+		
+		L.rawSet(o, n,f); // set loaded string in preload table, ready to execute when required
+		
+		L.pop(1);
+		
+	}
+
+// this is a bad interface, it is nothing but hacks for now
+
+	public static native void build_bridge() /*-{
+		$wnd.lua_create = $entry(function() {
+			return @wetgenes.jslua.client.JsLua::lua_create()();
+		});
+		$wnd.lua_get = $entry(function(L,n) {
+			return @wetgenes.jslua.client.JsLua::lua_get(Lmnj/lua/Lua;Ljava/lang/String;)(L,n);
+		});
+		$wnd.lua_set = $entry(function(L,n,v) {
+			@wetgenes.jslua.client.JsLua::lua_set(Lmnj/lua/Lua;Ljava/lang/String;Ljava/lang/String;)(L,n,v);
+		});
+		$wnd.lua_dostring = $entry(function(L,s,n) {
+			return @wetgenes.jslua.client.JsLua::lua_dostring(Lmnj/lua/Lua;Ljava/lang/String;Ljava/lang/String;)(L,s,n);
+		});
+		$wnd.lua_preloadstring = $entry(function(L,s,n) {
+			@wetgenes.jslua.client.JsLua::lua_preloadstring(Lmnj/lua/Lua;Ljava/lang/String;Ljava/lang/String;)(L,s,n);
+		});
 	}-*/;
 
 }
