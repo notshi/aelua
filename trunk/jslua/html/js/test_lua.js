674bf098448d5c6051bdea2465acde9909be60b1
krissd <krissd@09081f4e-5aab-11b8-2ae6-709035474cd6>
2010-11-08 18:44:50 +0000

    lets try and run some real codes

--- trunk/jslua/html/js/test_lua.js
+++ trunk/jslua/html/js/test_lua.js
@@ -3,11 +3,24 @@ var L; // this will be our lua state
 
 // lua has no fileaccess for module loading
 // so instead all modulefiles must be preloaded and shoved into package.preload
-// a little bit harsh or requesting lots of small files
+// a little bit harsh for requesting lots of small files
 // but it will do for now
 
 var preload_lua_list=[
-	{file:"/lua/testmod.lua",name:"testmod"}
+
+	{file:"/luac/hack.lua",name:"hack"},
+	{file:"/luac/hack/attr.lua",name:"hack.attr"},
+	{file:"/luac/hack/cell.lua",name:"hack.cell"},
+	{file:"/luac/hack/chardata.lua",name:"hack.chardata"},
+	{file:"/luac/hack/charfight.lua",name:"hack.charfight"},
+	{file:"/luac/hack/char.lua",name:"hack.char"},
+	{file:"/luac/hack/itemdata.lua",name:"hack.itemdata"},
+	{file:"/luac/hack/item.lua",name:"hack.item"},
+	{file:"/luac/hack/level.lua",name:"hack.level"},
+	{file:"/luac/hack/map.lua",name:"hack.map"},
+	{file:"/luac/hack/menu.lua",name:"hack.menu"},
+	{file:"/luac/hack/room.lua",name:"hack.room"}
+
 ];
 var preload_lua_idx=0;
 var preload_lua_func;
@@ -21,7 +34,7 @@ var v=preload_lua_list[preload_lua_idx];
 		
 window.lua_preloadstring(L,data,v.name);
 
-console.log(v.name + " : " + v.file);
+// console.log(v.name + " : " + v.file);
 
 		preload_lua_idx++;
 	}
@@ -49,26 +62,6 @@ console.log(v.name + " : " + v.file);
 
 var runstuff=function(){
 
-p=window.lua_dostring(L,'test=" test ".." to "..2 return "poop" ','maincode');
-console.log(p);
-
-//window.lua_set(L,'test','this is a test');
-
-var t=window.lua_get(L,'test');
-
-console.log(t);
-
-
-//console.log("preload");
-//window.lua_preloadstring(L,'local _G=_G ; module(...) _G.loadedtestmod="loaded test oh yes we have"','testmod');
-//console.log("postload");
-
-window.lua_dostring(L,'require("testmod")','precode');
-
-var t=window.lua_get(L,'loadedtestmod');
-console.log("got t");
-console.log(t);
-
 
 };
 
@@ -77,17 +70,15 @@ var runcheck;
 runcheck=function() {
 	if(window.lua_create) // wait for the java stuff to be ready
 	{
-console.log("loading");
+//console.log("loading");
 
 		L=window.lua_create();
-		
-console.log(window.test());
 		
 		preload_lua_func();
 	}
 	else
 	{
-console.log("waiting");
+//console.log("waiting");
 		window.setTimeout( runcheck , 1000); // try again later
 	}
 };
