181c9b81ee80e18d7f27cf102b5950da839457b2
krissd <krissd@09081f4e-5aab-11b8-2ae6-709035474cd6>
2010-11-10 15:08:21 +0000

    very raw terminal version of yarn, just run yarnterm.sh

---
+++ trunk/jslua/html/luac/yarnterm.lua
@@ -0,0 +1,39 @@
+package.path=package.path..";./?/init.lua"
+
+local yarn=require("yarn")
+
+local function keycode(code)
+
+	if code==65 then return "up" end
+	if code==66 then return "down" end
+	if code==68 then return "left" end
+	if code==67 then return "right" end
+	if code==32 then return "space" end
+	if code==27 then return "esc" end
+
+	return ""
+end
+
+local aesc=string.char(27) .. '['
+
+yarn.setup()
+yarn.update()
+print( aesc.."0;0H"..yarn.draw() )
+
+local exit=false
+while not exit do
+
+	local key_str=io.stdin:read(1)
+	
+	local key=keycode( key_str:byte() )
+	
+	if key_str=="q" then exit=true end
+
+	yarn.keypress(key_str,key,"down")
+	yarn.keypress(key_str,key,"up")
+	yarn.update()
+	print( aesc.."0;0H"..yarn.draw() )
+
+end
+
+
