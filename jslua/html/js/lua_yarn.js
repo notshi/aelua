ab11ef0b590cd3cb7636b56f9dd3c9758d488455
Kriss <kriss@XIXs.com>
2010-11-10 15:08:21 +0000

    very raw terminal version of yarn, just run yarnterm.sh

--- jslua/html/js/lua_yarn.js
+++ jslua/html/js/lua_yarn.js
@@ -8,18 +8,18 @@ var L; // this will be our lua state
 
 var preload_lua_list=[
 
-	{file:"luac/yarn.lua",				name:"yarn"},
-	{file:"luac/yarn/attr.lua",		name:"yarn.attr"},
-	{file:"luac/yarn/cell.lua",		name:"yarn.cell"},
-	{file:"luac/yarn/chardata.lua",	name:"yarn.chardata"},
+	{file:"luac/yarn/init.lua",			name:"yarn"},
+	{file:"luac/yarn/attr.lua",			name:"yarn.attr"},
+	{file:"luac/yarn/cell.lua",			name:"yarn.cell"},
+	{file:"luac/yarn/chardata.lua",		name:"yarn.chardata"},
 	{file:"luac/yarn/charfight.lua",	name:"yarn.charfight"},
-	{file:"luac/yarn/char.lua",		name:"yarn.char"},
-	{file:"luac/yarn/itemdata.lua",	name:"yarn.itemdata"},
-	{file:"luac/yarn/item.lua",		name:"yarn.item"},
+	{file:"luac/yarn/char.lua",			name:"yarn.char"},
+	{file:"luac/yarn/itemdata.lua",		name:"yarn.itemdata"},
+	{file:"luac/yarn/item.lua",			name:"yarn.item"},
 	{file:"luac/yarn/level.lua",		name:"yarn.level"},
 	{file:"luac/yarn/map.lua",			name:"yarn.map"},
-	{file:"luac/yarn/menu.lua",		name:"yarn.menu"},
-	{file:"luac/yarn/room.lua",		name:"yarn.room"}
+	{file:"luac/yarn/menu.lua",			name:"yarn.menu"},
+	{file:"luac/yarn/room.lua",			name:"yarn.room"}
 
 ];
 var preload_lua_idx=0;
