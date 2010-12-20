cad6108d43ba4f12e7832eda5c31174a7c05e0bb
Kriss <kriss@XIXs.com>
2010-12-20 22:19:20 +0000

    use google id rather than email as "email" so we can display it as profile name

---
+++ aelua/mods/mirror/README.txt
@@ -0,0 +1,13 @@
+
+import data from other sites
+
+This mostly a simple url fetch/cache combined with converters to turn multiple
+sources of data into a standard chunk of info.
+
+Imported data is stored with its URL as a key.
+
+This data is then processed and replicated off to other places,
+such as the mirror part of the waka.
+
+
+Under development...
---
+++ aelua/mods/mirror/lua/import.lua
@@ -0,0 +1,245 @@
+
+local wet_html=require("wetgenes.html")
+local url_esc=wet_html.url_esc
+
+local sys=require("wetgenes.aelua.sys")
+
+local json=require("json")
+local dat=require("wetgenes.aelua.data")
+local cache=require("wetgenes.aelua.cache")
+
+local users=require("wetgenes.aelua.users")
+
+local fetch=require("wetgenes.aelua.fetch")
+
+local img=require("wetgenes.aelua.img")
+
+local log=require("wetgenes.aelua.log").log -- grab the func from the package
+
+local wet_waka=require("wetgenes.waka")
+local wet_html=require("wetgenes.html")
+
+local wet_string=require("wetgenes.string")
+local str_split=wet_string.str_split
+local replace  =wet_string.replace
+local serialize=wet_string.serialize
+
+
+
+local math=math
+local string=string
+local table=table
+local os=os
+
+local ipairs=ipairs
+local pairs=pairs
+local tostring=tostring
+local tonumber=tonumber
+local type=type
+local pcall=pcall
+local loadstring=loadstring
+local require=require
+
+-- opts
+local opts_mods_note=(opts and opts.mods and opts.mods.note) or {}
+
+module("mirror.import")
+
+--------------------------------------------------------------------------------
+--
+-- serving flavour can be used to create a subgame of a different flavour
+-- make sure we incorporate flavour into the name of our stored data types
+--
+--------------------------------------------------------------------------------
+function kind(srv)
+	return "mirror.import" -- this mirror module is a site wide data cache
+end
+
+--------------------------------------------------------------------------------
+--
+-- Create a new local entity filled with initial data
+--
+--------------------------------------------------------------------------------
+function create(srv)
+
+	local ent={}
+	
+	ent.key={kind=kind(srv)} -- we will not know the key id until after we save
+	ent.props={}
+	
+	local p=ent.props
+	
+	p.created=srv.time
+	p.updated=srv.time
+	
+	p.data=""    -- the cached data returned from this url
+	p.type=""    -- the type of data this should be processed as
+	
+	dat.build_cache(ent) -- this just copies the props across
+	
+-- these are json only vars
+	local c=ent.cache
+
+	return check(srv,ent)
+end
+
+--------------------------------------------------------------------------------
+--
+-- check that entity has initial data and set any missing defaults
+-- the second return value is false if this is not a valid entity
+--
+--------------------------------------------------------------------------------
+function check(srv,ent)
+
+	local ok=true
+
+	local c=ent.cache
+			
+	return ent,ok
+end
+
+
+--------------------------------------------------------------------------------
+--
+-- manifest a meta comment cache, there is only one of these per url
+-- so we can use that as its key
+-- most data is kept in its ent.cache.cache table
+--
+--------------------------------------------------------------------------------
+function manifest(srv,url,t)
+
+	local ent
+	
+	local fill=false
+	
+	if not ent then
+		ent=create(srv)
+		ent.key.id=url
+		ent=get(srv,ent,t) -- prevent manifest recursion by passing in ent
+	end
+	
+	if not ent then
+		ent=create(srv)
+		fill=true
+	end
+	
+	if fill then
+		ent.key.id=url -- force id which is page name string
+		ent.cache.id=url -- copy here
+	end
+	
+	return (check(serv,ent)) -- wrap in () to just return the ent
+end
+
+--------------------------------------------------------------------------------
+--
+-- Save to database
+-- this calls check before putting and does not put if check says it is invalid
+-- build_props is called so code should always be updating the cache values
+--
+--------------------------------------------------------------------------------
+function put(srv,ent,t)
+
+	t=t or dat -- use transaction?
+
+	local _,ok=check(srv,ent) -- check that this is valid to put
+	if not ok then return nil end
+
+	dat.build_props(ent)
+	local ks=t.put(ent)
+	
+	if ks then
+		ent.key=dat.keyinfo( ks ) -- update key with new id
+		dat.build_cache(ent)
+	end
+
+	return ks -- return the keystring which is an absolute name
+end
+
+
+--------------------------------------------------------------------------------
+--
+-- Load from database, pass in id or entity
+-- the props will be copied into the cache
+--
+--------------------------------------------------------------------------------
+function get(srv,id,t)
+
+	if type(id)=="string" then -- auto manifest by url
+		return manifest(srv,id,t)
+	end
+	
+	local ent=id
+	if type(ent)~="table" then -- get by id
+		ent=create(srv)
+		ent.key.id=id
+	end
+	
+	t=t or dat -- use transaction?
+	
+	if not t.get(ent) then return nil end	
+	dat.build_cache(ent)
+	
+	return check(srv,ent)
+end
+
+--------------------------------------------------------------------------------
+--
+-- get - update - put
+--
+-- f must be a function that changes the entity and returns true on success
+-- id can be an id or an entity from which we will get the id
+--
+--------------------------------------------------------------------------------
+function update(srv,id,f)
+
+	if type(id)=="table" then id=id.key.id end -- can turn an entity into an id
+		
+	for retry=1,10 do
+		local mc={}
+		local t=dat.begin()
+		local e=get(srv,id,t)
+		if e then
+			what_memcache(srv,e,mc) -- the original values
+			e.cache.updated=srv.time -- the function can change this change if it wishes
+			if not f(srv,e) then t.rollback() return false end -- hard fail
+			check(srv,e) -- keep consistant
+			if put(srv,e,t) then -- entity put ok
+				if t.commit() then -- success
+					what_memcache(srv,e,mc) -- the new values
+					fix_memcache(srv,mc) -- change any memcached values we just adjusted
+					return e -- return the adjusted entity
+				end
+			end
+		end
+		t.rollback() -- undo everything ready to try again
+	end
+	
+end
+
+
+--------------------------------------------------------------------------------
+--
+-- given an entity return or update a list of memcache keys we should recalculate
+-- this list is a name->bool lookup
+--
+--------------------------------------------------------------------------------
+function what_memcache(srv,ent,mc)
+	local mc=mc or {} -- can supply your own result table for merges	
+	local c=ent.cache
+	
+	return mc
+end
+
+--------------------------------------------------------------------------------
+--
+-- fix the memcache items previously produced by what_memcache
+-- probably best just to delete them so they will automatically get rebuilt
+--
+--------------------------------------------------------------------------------
+function fix_memcache(srv,mc)
+	for n,b in pairs(mc) do
+		cache.del(n)
+	end
+end
+
