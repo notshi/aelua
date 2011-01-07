99e35d85ae4eb70bd5da00a58922ce453e952c6b
Kriss <kriss@XIXs.com>
2011-01-07 03:44:29 +0000

    added a script to install or update yarn from the internets

---
+++ jslua/html/luac/yarnupdate.sh
@@ -0,0 +1,20 @@
+
+echo "This will apt-get install lua(5.1) and svn if needed and then checkout the latest version of yarn to the current directory, please read this script file to verify it isn't doing anything bad before running it and make sure you are inside a directory you wish to install yarn in before running. KTHXBYE"
+
+type -P lua &>/dev/null || {
+echo "installing lua using sudo apt-get"
+sudo apt-get install lua5.1 subversion
+}
+
+type -P svn &>/dev/null || {
+echo "installing svn using sudo apt-get"
+sudo apt-get install subversion
+}
+
+
+echo "getting or updating yarn from google code"
+svn checkout https://aelua.googlecode.com/svn/trunk/jslua/html/luac .
+cd yarn
+
+
+echo "you may now run ./yarnterm.sh to play"
