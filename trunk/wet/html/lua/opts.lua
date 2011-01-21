048f9b4425d25e5ca29cf5a82f5aa08254ebc654
krissd <krissd@09081f4e-5aab-11b8-2ae6-709035474cd6>
2011-01-21 05:13:34 +0000

    version bump

--- trunk/wet/html/lua/opts.lua
+++ trunk/wet/html/lua/opts.lua
@@ -17,7 +17,7 @@ local type=type
 
 module("opts")
 
-bootstrapp_version=20110119 -- hand bump to todays date on release
+bootstrapp_version=20110121 -- hand bump to todays date on release
 
 urls={}
 
