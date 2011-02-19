ecf8e817395e9ad0e595096a46c043ed7acd86b2
krissd <krissd@09081f4e-5aab-11b8-2ae6-709035474cd6>
2011-02-19 22:57:25 +0000

    bump

--- trunk/boot-str/html/lua/opts.lua
+++ trunk/boot-str/html/lua/opts.lua
@@ -18,7 +18,7 @@ local type=type
 module("opts")
 
 --Machine generated data. Read between the lines, but do not edit.
-bootstrapp_version=20110207.14
+bootstrapp_version=20110219.95
 --Machine generated data. Read between the lines, but do not edit.
 
 urls={}
