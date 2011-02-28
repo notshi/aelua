93a27076d4ecabb0d9a49986891bf0c0127e433d
Kriss <kriss@XIXs.com>
2011-02-28 14:19:22 +0000

    bugfix minimal url escape as it requires more characters...

--- boot-str/html/lua/opts.lua
+++ boot-str/html/lua/opts.lua
@@ -18,7 +18,7 @@ local type=type
 module("opts")
 
 --Machine generated data. Read between the lines, but do not edit.
-bootstrapp_version=20110227.86
+bootstrapp_version=20110228.59
 --Machine generated data. Read between the lines, but do not edit.
 
 urls={}
