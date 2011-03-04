279323b8b457726f7a412e389452563471220da7
krissd <krissd@09081f4e-5aab-11b8-2ae6-709035474cd6>
2011-03-04 17:57:49 +0000

    Added facebook login option. You will need to make an app and type in the magic numbers just as you do for twitter login.

--- trunk/boot-str/html/lua/opts.lua
+++ trunk/boot-str/html/lua/opts.lua
@@ -18,7 +18,7 @@ local type=type
 module("opts")
 
 --Machine generated data. Read between the lines, but do not edit.
-bootstrapp_version=20110302.96
+bootstrapp_version=20110304.74
 --Machine generated data. Read between the lines, but do not edit.
 
 mail={}
