16e7d2df2bc851e88c3bf1dc59c6751495df0d1d
krissd <krissd@09081f4e-5aab-11b8-2ae6-709035474cd6>
2011-03-15 14:50:11 +0000

    changed how twitter comments are split from long comments

--- trunk/boot-str/html/lua/opts.lua
+++ trunk/boot-str/html/lua/opts.lua
@@ -18,7 +18,7 @@ local type=type
 module("opts")
 
 --Machine generated data. Read between the lines, but do not edit.
-bootstrapp_version=20110309.11
+bootstrapp_version=20110315.61
 --Machine generated data. Read between the lines, but do not edit.
 
 mail={}
