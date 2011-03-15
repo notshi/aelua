9cab238dc6ce36ecb022e8c989791ab7be32d157
krissd <krissd@09081f4e-5aab-11b8-2ae6-709035474cd6>
2011-03-15 17:02:42 +0000

    ok try again with the twitterness

--- trunk/boot-str/html/lua/opts.lua
+++ trunk/boot-str/html/lua/opts.lua
@@ -18,7 +18,7 @@ local type=type
 module("opts")
 
 --Machine generated data. Read between the lines, but do not edit.
-bootstrapp_version=20110315.61
+bootstrapp_version=20110315.7
 --Machine generated data. Read between the lines, but do not edit.
 
 mail={}
